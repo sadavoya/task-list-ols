34742d8962ea109dffa49a8b5d70d6c3f435271d
A Sadavoy <sadavoya@gmail.com>
2020-11-04 15:56:45 -0500

    Remove Todo List title from App

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
         <div className="col-10 mx-auto col-md-8 mt-4">
           <h3 className="text-capitalize text-center">todo input</h3>
           <TodoInput />
-          <h3 className="text-capitalize text-center">todo list</h3>
           <TodoList />
         </div>
       </div>
