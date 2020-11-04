5ef3fc6f4aec5ac130480d66c8f9ca1cb83c13fb
A Sadavoy <sadavoya@gmail.com>
2020-11-04 15:57:18 -0500

    Structure Todo List: * list * title * todo item placeholder * clear list button

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,10 +4,13 @@ import TodoItem from "./TodoItem";
 export default class TodoList extends Component {
   render() {
     return (
-      <div>
-        <h4>hello from todo list</h4>
+      <ul className="list-group my-5">
+        <h3 className="text-capitalize text-center">todo list</h3>
         <TodoItem />
-      </div>
+        <button className="btn btn-danger btn-block text-capitalize mt-5">
+          clear list
+        </button>
+      </ul>
     );
   }
 }
