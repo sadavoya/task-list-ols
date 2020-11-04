4f233acc1f11461cd0f624e34f62c9a310ecbe58
A Sadavoy <sadavoya@gmail.com>
2020-11-04 15:48:29 -0500

    Add structure to TodoInput: * card * icon * text box * button

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -2,6 +2,29 @@ import React, { Component } from "react";
 
 export default class TodoInput extends Component {
   render() {
-    return <div>hello from todo input</div>;
+    return (
+      <div className="card card-body my-3">
+        <form action="">
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <div className="input-group-text bg-primary text-white">
+                <i className="fas fa-book" />
+              </div>
+            </div>
+            <input
+              type="text"
+              className="form-control text-capitalize"
+              placeholder="add a todo item"
+            />
+          </div>
+          <button
+            type="submit"
+            className="btn btn-block text-capitalize btn-primary mt-3"
+          >
+            add item
+          </button>
+        </form>
+      </div>
+    );
   }
 }
