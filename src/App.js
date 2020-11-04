cd9cab771dbb8bcfb4e11a38d9550b2dea6823d8
A Sadavoy <sadavoya@gmail.com>
2020-11-04 15:36:28 -0500

    Update App: * Import components * Import bootstrap * Import uuid * Add components for TodoInput and TodoList

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import uuid from "uuid";
+
 function App() {
-  return <div>hello from app</div>;
+  return (
+    <div>
+      <h1>hello from app</h1>
+      <TodoInput />
+      <TodoList />
+    </div>
+  );
 }
 
 export default App;
