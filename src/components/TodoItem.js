51ddb0cc48560de4f2eea693cf52793e58af055b
A Sadavoy <sadavoya@gmail.com>
2020-11-04 16:07:23 -0500

    Structure TodoItem: * li * title * icons

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,6 +2,18 @@ import React, { Component } from "react";
 
 export default class TodoItem extends Component {
   render() {
-    return <div>hello from todo item</div>;
+    return (
+      <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
+        <h6 className="text-capitalize">title</h6>
+        <div className="todo-icon">
+          <span className="mx-2 text-success">
+            <i className="fas fa-pen" />
+          </span>
+          <span className="mx-2 text-danger">
+            <i className="fas fa-trash" />
+          </span>
+        </div>
+      </li>
+    );
   }
 }
