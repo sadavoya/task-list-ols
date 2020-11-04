4b4695c969692c548e0a9f68dc196e7cbfd813f2
A Sadavoy <sadavoya@gmail.com>
2020-11-04 16:13:40 -0500

    Convert App to a class

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
+import React, { Component } from "react";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import uuid from "uuid";
 
-function App() {
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-10 mx-auto col-md-8 mt-4">
-          <h3 className="text-capitalize text-center">todo input</h3>
-          <TodoInput />
-          <TodoList />
+export default class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto col-md-8 mt-4">
+            <h3 className="text-capitalize text-center">todo input</h3>
+            <TodoInput />
+            <TodoList />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default App;
