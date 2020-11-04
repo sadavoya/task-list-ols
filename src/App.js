58364fcac8ac0ad582f39e2fdde0c72ae982a7f7
A Sadavoy <sadavoya@gmail.com>
2020-11-04 16:21:30 -0500

    Update App: * add state * add handleChange * pass item and handleChange to TodoInput

--- src/App.js
+++ src/App.js
@@ -6,13 +6,25 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import uuid from "uuid";
 
 export default class App extends Component {
+  state = {
+    items: [],
+    id: 0,
+    item: "",
+    editItem: false,
+  };
+  handleChange = (e) => {
+    this.setState({ item: e.target.value });
+  };
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-10 mx-auto col-md-8 mt-4">
             <h3 className="text-capitalize text-center">todo input</h3>
-            <TodoInput />
+            <TodoInput
+              item={this.state.item}
+              handleChange={this.handleChange}
+            />
             <TodoList />
           </div>
         </div>
