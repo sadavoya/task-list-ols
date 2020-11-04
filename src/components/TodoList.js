343252d4959029492bf67fccafeac5099cd1a3dc
A Sadavoy <sadavoya@gmail.com>
2020-11-04 16:53:09 -0500

    Update TodoList: * display all items in list

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,10 +3,13 @@ import TodoItem from "./TodoItem";
 
 export default class TodoList extends Component {
   render() {
+    const { items } = this.props;
     return (
       <ul className="list-group my-5">
         <h3 className="text-capitalize text-center">todo list</h3>
-        <TodoItem />
+        {items.map((item) => {
+          return <TodoItem key={item.id} item={item} />;
+        })}
         <button className="btn btn-danger btn-block text-capitalize mt-5">
           clear list
         </button>
