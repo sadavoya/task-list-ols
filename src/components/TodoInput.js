90909d1fed98df2ac91fdf5ef79fd60257dc9158
A Sadavoy <sadavoya@gmail.com>
2020-11-05 07:57:40 -0500

    Update TodoInput: * receive editItem * update button colour and text based on editItem

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 export default class TodoInput extends Component {
   render() {
-    const { item, handleChange, handleAddItem } = this.props;
+    const { item, editItem, handleChange, handleAddItem } = this.props;
 
     return (
       <div className="card card-body my-3">
@@ -23,9 +23,13 @@ export default class TodoInput extends Component {
           </div>
           <button
             type="submit"
-            className="btn btn-block text-capitalize btn-primary mt-3"
+            className={
+              editItem
+                ? "btn btn-block text-capitalize mt-3 btn-success"
+                : "btn btn-block text-capitalize mt-3 btn-primary"
+            }
           >
-            add item
+            {editItem ? "edit item" : "add item"}
           </button>
         </form>
       </div>
