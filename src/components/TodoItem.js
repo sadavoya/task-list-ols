669e13e6f3436825612d0c9c1129184c534207d7
A Sadavoy <sadavoya@gmail.com>
2020-11-05 07:39:40 -0500

    Update TodoItem: * recieve handleDelete * call handleDelete from trash icon span

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,15 +2,15 @@ import React, { Component } from "react";
 
 export default class TodoItem extends Component {
   render() {
-    const { id, title } = this.props.item;
+    const { item, handleDelete } = this.props;
     return (
       <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
-        <h6 className="text-capitalize">{title}</h6>
+        <h6 className="text-capitalize">{item.title}</h6>
         <div className="todo-icon">
           <span className="mx-2 text-success">
             <i className="fas fa-pen" />
           </span>
-          <span className="mx-2 text-danger">
+          <span className="mx-2 text-danger btn" onClick={handleDelete}>
             <i className="fas fa-trash" />
           </span>
         </div>
