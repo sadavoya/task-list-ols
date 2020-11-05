0720ea8f264fcfa22e5726230772cf49076cbd25
A Sadavoy <sadavoya@gmail.com>
2020-11-05 07:26:15 -0500

    Update TodoList: * receive clearList * call clearList from clear button

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,14 +3,17 @@ import TodoItem from "./TodoItem";
 
 export default class TodoList extends Component {
   render() {
-    const { items } = this.props;
+    const { items, clearList } = this.props;
     return (
       <ul className="list-group my-5">
         <h3 className="text-capitalize text-center">todo list</h3>
         {items.map((item) => {
           return <TodoItem key={item.id} item={item} />;
         })}
-        <button className="btn btn-danger btn-block text-capitalize mt-5">
+        <button
+          className="btn btn-danger btn-block text-capitalize mt-5"
+          onClick={clearList}
+        >
           clear list
         </button>
       </ul>
